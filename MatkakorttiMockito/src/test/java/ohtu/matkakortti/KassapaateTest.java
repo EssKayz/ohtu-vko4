5eaf0fd8143f73a97df1915da3352b6f96401667
EssKayz <esskay@protonmail.com>
2018-11-25 09:43:21 +0200

    Exercise 5

--- MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ MatkakorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -1,4 +1,3 @@
-
 package ohtu.matkakortti;
 
 import ohtu.matkakortti.Matkakortti;
@@ -13,21 +12,41 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KassapaateTest {
-    
+
     Kassapaate kassa;
     Matkakortti kortti;
+
+//	kassapäätteen metodin lataa kutsu lisää matkakortille ladattavan rahamäärän käyttäen kortin metodia lataa jos ladattava summa on positiivinen
+//	kassapäätteen metodin lataa kutsu ei tee matkakortille mitään jos ladattava summa on negatiivinen
     
     @Before
     public void setUp() {
         kassa = new Kassapaate();
         kortti = mock(Matkakortti.class);
     }
+
+    @Test
+    public void kassallaVoiLadataRahaa() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.lataa(kortti, 10);
+        
+        verify(kortti, times(1)).lataa(10);
+    }
     
+        @Test
+    public void kassallaEiVoiLadataNegatiivistaRahaa() {
+        when(kortti.getSaldo()).thenReturn(10);
+        kassa.lataa(kortti, 10);
+
+        verify(kortti, times(0)).lataa(-10);
+    }
+    
+
     @Test
     public void kortiltaVelotetaanHintaJosRahaaOn() {
         when(kortti.getSaldo()).thenReturn(10);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti).osta(eq(Kassapaate.HINTA));
     }
@@ -36,9 +55,9 @@ public class KassapaateTest {
     public void kortiltaEiVelotetaJosRahaEiRiita() {
         when(kortti.getSaldo()).thenReturn(4);
         kassa.ostaLounas(kortti);
-        
+
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
 }
---
+++ Verkkokauppa1/src/test/java/ohtu/verkkokauppa/VerkkoKauppaTest.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+import ohtu.verkkokauppa.Kauppa;
+import ohtu.verkkokauppa.Pankki;
+import ohtu.verkkokauppa.Tuote;
+import ohtu.verkkokauppa.Varasto;
+import ohtu.verkkokauppa.Viitegeneraattori;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author ColdFish
+ */
+public class VerkkoKauppaTest {
+
+    Pankki pankki;
+    Viitegeneraattori viite;
+    Varasto varasto;
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.tilimaksu("pekka", "12345");
+
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
+    }
+
+    @Test
+    public void ostetaanMaitoJaJuusto() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "54321");
+
+        verify(pankki).tilisiirto("pekka", 42, "54321", "33333-44455", 15);
+    }
+
+    @Test
+    public void ostetaanMaitoJaPoistetaanJuusto() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.poistaKorista(2);
+        k.tilimaksu("pekka", "54321");
+
+        verify(pankki).tilisiirto("pekka", 42, "54321", "33333-44455", 5);
+    }
+
+    @Test
+    public void ostetaanMaitoJaLoppunutJuusto() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        when(varasto.saldo(2)).thenReturn(0);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "54321");
+
+        verify(pankki).tilisiirto("pekka", 42, "54321", "33333-44455", 5);
+    }
+
+    @Test
+    public void ostetaanKaksiJuustoa() {
+        when(viite.uusi()).thenReturn(42);
+
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(2, "juusto", 10));
+
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "juusto", 10));
+
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);
+        k.lisaaKoriin(2);
+        k.tilimaksu("pekka", "564321");
+
+        verify(pankki).tilisiirto("pekka", 42, "564321", "33333-44455", 20);
+    }
+
+}
